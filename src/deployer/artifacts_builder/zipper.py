c66cc9652354bf2cbb239515fccbe7d78724c9cf
Caio Cominato <caiopetrellicominato@gmail.com>
2023-05-31 13:17:20 +0000

    Added include_root to zip artifacts

--- src/deployer/artifacts_builder/zipper.py
+++ src/deployer/artifacts_builder/zipper.py
@@ -2,7 +2,7 @@ import os
 import shutil
 import zipfile
 
-def create_zip_from_folder(source, target):
+def create_zip_from_folder(source, target, include_root=False):
     # Create the target directory if it does not exist
     target_dir = os.path.dirname(target)
     if not os.path.exists(target_dir):
@@ -14,16 +14,19 @@ def create_zip_from_folder(source, target):
 
     try:
         # Copy the source folder to the temporary folder
-        shutil.copytree(source, os.path.join(temp_folder, os.path.basename(source)))
+        base_folder = os.path.basename(source)
+        shutil.copytree(source, os.path.join(temp_folder, base_folder))
 
         # Create the zip file
         with zipfile.ZipFile(target, 'w', zipfile.ZIP_DEFLATED) as zipf:
             for root, dirs, files in os.walk(temp_folder):
                 for file in files:
                     file_path = os.path.join(root, file)
-                    zipf.write(file_path, os.path.relpath(file_path, temp_folder))
+                    zip_path = os.path.relpath(file_path, temp_folder)
+                    if not include_root:
+                        zip_path = zip_path.replace(f'{base_folder}/', '')
+                    zipf.write(file_path, zip_path)
 
     finally:
         # Clean up the temporary folder
         shutil.rmtree(temp_folder)
-
